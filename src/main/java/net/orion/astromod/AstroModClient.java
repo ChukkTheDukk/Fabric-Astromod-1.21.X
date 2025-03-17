362fe4e02a50a47186db905a7b206c83ac89b3a3
ChukkTheDukk <ryanmatthewstevenson@gmail.com>
2025-03-16 20:08:39 -0600

    Initial commit

---
+++ src/main/java/net/orion/astromod/AstroModClient.java
@@ -0,0 +1,10 @@
+package net.orion.astromod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AstroModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
